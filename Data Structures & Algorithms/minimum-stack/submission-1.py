d482e66d902cfeb4608b310dca8cff4330950cff
Justin Yoo <143750074+justineyoo1@users.noreply.github.com>
2026-04-23 13:44:34 -0400

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,31 @@
+class MinStack:
+    #stack is a list under the hood
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        
+        if len(self.minStack) == 0:
+            self.minStack.append(val)
+
+        elif self.minStack and val <= self.minStack[-1]:
+            self.minStack.append(val)
+
+    def pop(self) -> None:
+        if self.stack[-1] == self.minStack[-1]:
+            self.minStack.pop()
+        self.stack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+
+        
